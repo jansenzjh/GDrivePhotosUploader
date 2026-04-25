ded35c5fd3f99eeea914d6b503da83740274552e
Jansen <jansenzjh@users.noreply.github.com>
2026-04-25 21:35:26 +0900

    Initial Commit

---
+++ GDrivePhotosUploader/GDrivePhotosUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDrivePhotosUploaderApp.swift
+//  GDrivePhotosUploader
+//
+//  Created by Jianheng Zhang on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct GDrivePhotosUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
